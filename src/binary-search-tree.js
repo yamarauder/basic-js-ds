bd78982f15edd430a63fef3b06a449ba823aa056
Valeriy Hurkov <yamarauder@mail.ru>
2022-05-02 13:17:37 +0300

    add solution binary-search

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,42 +6,161 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinarySearchTree {
 
+  constructor() {
+    this.roo = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.roo;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(date) {
+
+    this.roo = addWithin(this.roo, date);
+
+    function addWithin(node, value) {
+      if (!node) {
+        return new Node(value);
+      }
+      if (node.data === value) {
+        return node;
+      }
+      if (value < node.data) {
+        node.left = addWithin(node.left, value);
+      } else {
+        node.right = addWithin(node.right, value);
+      }
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+
+    return searchWithin(this.roo, data);
+
+    function searchWithin(node, value) {
+
+      if (!node) {
+        return false;
+      }
+
+      if (node.data === value) {
+        return true;
+      }
+
+      return (value < node.data) ? searchWithin(node.left, value) : searchWithin(node.right, value);
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(date) {
+
+    return findWithin(this.roo, date);
+
+    function findWithin(node, value) {
+
+      if (!node) {
+        return null;
+      }
+
+      if (node.data === value) {
+        return node;
+      }
+
+      return (value < node.data) ? findWithin(node.left, value) : findWithin(node.right, value);
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.roo = removeWithin(this.roo, data);
+
+    function removeWithin(node, value) {
+
+      if (!node) {
+        return null;
+      }
+
+
+      if (value < node.data) {
+        node.left = removeWithin(node.left, value);
+        return node;
+      } else if (node.data < value) {
+        node.right = removeWithin(node.right, value);
+        return node;
+      } else {
+
+        if (!node.left && !node.right) {
+          return null;
+        }
+
+        if (!node.left) {
+          node = node.right;
+          return node;
+        }
+        if (!node.right) {
+          node = node.left;
+          return node;
+        }
+
+
+        let minFromRight = node.right;
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left;
+        }
+
+        node.data = minFromRight.data;
+        node.right = removeWithin(node.right, minFromRight.data);
+        return node;
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    return minAmount(this.roo);
+
+    function minAmount(node) {
+
+      if (!node) { // если у дерева нет узлов
+        return null;
+      }
+
+      let minFrom = node.left;
+      while (minFrom.left) {
+        minFrom = minFrom.left;
+      }
+      return minFrom.data;
+    }
   }
 
+
+
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return maxAmount(this.roo);
+
+    function maxAmount(node) {
+
+      if (!node) { // если у дерева нет узлов
+        return null;
+      }
+
+      let maxFrom = node.right;
+      while (maxFrom.right) {
+        maxFrom = maxFrom.right;
+      }
+      return maxFrom.data;
+    }
   }
+
 }
 
 module.exports = {
